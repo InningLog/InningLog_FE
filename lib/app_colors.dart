78208aed0f1b449f3bedaafcba04fded665b0d7d
Ryujee Kim <130418731+ryujeeKim@users.noreply.github.com>
2025-06-06 18:58:20 +0900

    캘린더 추가

--- lib/app_colors.dart
+++ lib/app_colors.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  static const Color primary100 = Color(0xFFF9FCF1);
   static const Color primary200 = Color(0xFFF0F8E0);
   static const Color primary700 = Color(0xFF94C32C);
   static const Color primary800 = Color(0xFF536E19);
   static const Color gray700 = Color(0xFF8F8F8F);
   static const Color gray600 = Color(0xFFA9A9A9);
   static const Color gray50 = Color(0xFFFDFDFD);
+  static const Color gray100 = Color(0xFFF8F8F8);
+  static const Color gray200 = Color(0xFFEFEFEF);
+  static const Color red100 = Color(0xFFFDF6F5);
+  static const Color red700 = Color(0xFFDC6D65);
 
 
 }
--- lib/screens/diary_page.dart
+++ lib/screens/diary_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import '../app_colors.dart';
 import '../widgets/common_header.dart';
+import 'package:table_calendar/table_calendar.dart';
+
 
 class DiaryPage extends StatefulWidget {
   const DiaryPage({super.key});
@@ -12,8 +14,8 @@ class DiaryPage extends StatefulWidget {
 
 class _DiaryPageState extends State<DiaryPage> {
   int _selectedIndex = 0;
+  String? selectedFilter; // 선택된 버튼 저장 (처음에는 null → 선택 없음)
 
-  //상단 탭바
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,39 +33,64 @@ class _DiaryPageState extends State<DiaryPage> {
                   Expanded(child: _buildTabButton(index: 0, label: '캘린더')),
                   Expanded(child: _buildTabButton(index: 1, label: '모아보기')),
                 ],
-               ),
-
-              ), //승/패/무 필터 버튼
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 19),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    _buildFilterButton('승리'),
-                    _buildFilterButton('패배'),
-                    _buildFilterButton('무승부'),
-                  ],
-                ),
               ),
+            ),
+
+            //승/패/무 필터 버튼
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 12),
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 19),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  _buildMonthHeader(),
+                  _buildFilterButton('승리'),
+                  _buildFilterButton('패배'),
+                  _buildFilterButton('무승부'),
                 ],
               ),
             ),
 
+            // 월 헤더
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 1),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 0),
+                  _buildMonthHeader(),
+                  const SizedBox(height: 28.5),
+                  TableCalendar(
+                    firstDay: DateTime.utc(2020, 1, 1),
+                    lastDay: DateTime.utc(2030, 12, 31),
+                    focusedDay: DateTime.now(),
+                    calendarStyle: CalendarStyle(
+                      todayDecoration: BoxDecoration(
+                        color: AppColors.primary200,
+                        shape: BoxShape.circle,
+                      ),
+                      selectedDecoration: BoxDecoration(
+                        color: AppColors.primary700,
+                        shape: BoxShape.circle,
+                      ),
+                      selectedTextStyle: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Pretendard',
+                      ),
+                      todayTextStyle: TextStyle(
+                        color: AppColors.primary800,
+                        fontFamily: 'Pretendard',
+                      ),
+                    ),
+                    headerVisible: false, // 너는 위에 "5월 ⌄" 따로 구현했으니 header 숨기면 됨
+                  )
 
-          ],
+                ],
+              ),
             ),
+          ],
+        ),
       ),
     );
   }
-
-
 
   Widget _buildTabButton({required int index, required String label}) {
     final isSelected = _selectedIndex == index;
@@ -73,14 +100,11 @@ class _DiaryPageState extends State<DiaryPage> {
           _selectedIndex = index;
         });
       },
-      //탭바 디자인 요소
       child: Container(
-        //전체 컨테이너
         width: 195,
         alignment: Alignment.center,
         padding: const EdgeInsets.symmetric(vertical: 0),
         decoration: BoxDecoration(
-          //배경색
           color: isSelected ? AppColors.primary200 : Colors.transparent,
           border: Border(
             bottom: BorderSide(
@@ -91,8 +115,6 @@ class _DiaryPageState extends State<DiaryPage> {
           borderRadius: BorderRadius.circular(0),
         ),
         child: Text(
-
-          //탭바 텍스트 요소
           label,
           style: TextStyle(
             fontFamily: 'Pretendard',
@@ -100,62 +122,101 @@ class _DiaryPageState extends State<DiaryPage> {
             letterSpacing: -0.26,
             height: 1.5,
             fontWeight: isSelected ? FontWeight.w600 : FontWeight.w500,
-            color: isSelected ?AppColors.primary800 : AppColors.gray700,
+            color: isSelected ? AppColors.primary800 : AppColors.gray700,
           ),
         ),
       ),
     );
   }
-}
-      //승패무 버튼 디자인
-      Widget _buildFilterButton(String label) {
-        return Container(
-          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 44), // 8px 44px 비율
-          decoration: BoxDecoration(
-            color: AppColors.gray50,
-            borderRadius: BorderRadius.circular(16),
-            border: Border.all(
-              color:  AppColors.gray600,
-              width: 1,
-            ),
-          ),
-          child: Text(
-            label,
-            style: const TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w600,
-              color: AppColors.gray700,
-              fontFamily: 'Pretendard',
-              letterSpacing: -0.12,
-              height: 1.5,
-            ),
-          ),
-        );
+
+  Widget _buildFilterButton(String label) {
+    final isSelected = selectedFilter == label;
+
+    // 버튼별 색 지정
+    Color backgroundColor = AppColors.gray50;
+    Color borderColor = AppColors.gray600;
+    Color textColor = AppColors.gray700;
+
+    if (isSelected) {
+      if (label == '승리') {
+        backgroundColor = AppColors.primary100;
+        borderColor = AppColors.primary700;
+        textColor = AppColors.primary700;
+      } else if (label == '패배') {
+        backgroundColor = AppColors.red100;
+        borderColor = AppColors.red700;
+        textColor = AppColors.red700;
+      } else if (label == '무승부') {
+        backgroundColor = AppColors.gray200; // 나중에 무승부 색 변경 가능
+        borderColor = AppColors.gray700;
+        textColor = AppColors.gray700;
       }
-        Widget _buildMonthHeader() {
-          final now = DateTime.now();
-          final currentMonth = '${now.month}월';
+    }
 
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Row(
-              children: [
-                Text(
-                  currentMonth,
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w700,
-                    color: Color(0xFF272727),
-                  ),
-                ),
-                const SizedBox(width: 4),
-                SvgPicture.asset(
-                  'assets/icons/Home_black.svg',
-                  width: 6,
-                  height: 11
-                ),
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          if (selectedFilter == label) {
+            // 이미 선택된 버튼을 다시 누르면 해제
+            selectedFilter = null;
+          } else {
+            // 새로운 버튼 선택
+            selectedFilter = label;
+          }
+        });
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 44),
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(
+            color: borderColor,
+            width: 1,
+          ),
+        ),
+        child: Text(
+          label,
+          style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w600,
+            color: textColor,
+            fontFamily: 'Pretendard',
+            letterSpacing: -0.12,
+            height: 1.5,
+          ),
+        ),
+      ),
+    );
+  }
 
-              ],
+  Widget _buildMonthHeader() {
+    final now = DateTime.now();
+    final currentMonth = '${now.month}월';
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        children: [
+          Text(
+            currentMonth,
+            style: const TextStyle(
+              fontFamily: 'Pretendard',
+              fontSize: 19,
+              letterSpacing: -0.19,
+              height: 1.444,
+              fontWeight: FontWeight.w700,
+              color: Color(0xFF000000),
             ),
-          );
-        }
+          ),
+          const SizedBox(width: 9),
+          SvgPicture.asset(
+            'assets/icons/month_move.svg',
+            width: 6,
+            height: 11,
+          ),
+        ],
+      ),
+    );
+  }
+}
