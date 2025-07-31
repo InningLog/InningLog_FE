90c0fce84e04f5e3b54a2ffbd033b2eaa8703b75
Ryujee Kim <130418731+ryujeeKim@users.noreply.github.com>
2025-08-01 05:48:13 +0900

    앱키 숨김

--- lib/analytics/analytics.dart
+++ lib/analytics/analytics.dart
@@ -1,11 +1,15 @@
 import 'package:inninglog/analytics/amplitude_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AnalyticsService {
   late final AmplitudeFlutter _amplitude;
 
-  void init() {
+  Future<void> init() async {
+    await dotenv.load();
     _amplitude = AmplitudeFlutter.getInstance(instanceName: "default");
-    _amplitude.init("821c8925a751c008310e896ad437b1bc");
+
+    final amplitudeKey = dotenv.env['AMPLITUDE_API_KEY'];
+    _amplitude.init(amplitudeKey!); // amplitude → _amplitude (오타 주의)
     _amplitude.trackingSessionEvents(true);
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -24,15 +24,21 @@ import 'dart:io' show Platform;
 import 'analytics/analytics.dart';
 import 'package:inninglog/analytics/amplitude_flutter.dart';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
+
 
 final analytics = AnalyticsService();
 
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   analytics.init();
+  await dotenv.load();
 
-
+  KakaoSdk.init(
+    nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY']!,
+  );
 
   runApp(const InningLogApp());
 }
