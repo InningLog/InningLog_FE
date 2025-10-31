fd2edf0c1c0f9777ed19df2f426fcb68ffe2f3b6
ryujeeKim <ryujee0512@naver.com>
2025-10-31 15:36:18 +0900

    [Feat]알람 화면 작업 완료

--- lib/widgets/common_header.dart
+++ lib/widgets/common_header.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
+import '../screens/alarm_page.dart';
+
 class CommonHeader extends StatelessWidget {
   final String title;
   final VoidCallback? onAlarmPressed;
@@ -42,16 +44,12 @@ class CommonHeader extends StatelessWidget {
               'assets/icons/Alarm.svg',
               width: 18.05,
             ),
-            onPressed: () {
-              Fluttertoast.showToast(
-                msg: "아직 개발 중이에요 ㅠ.ㅠ",
-                toastLength: Toast.LENGTH_SHORT,
-                gravity: ToastGravity.BOTTOM,
-                backgroundColor: Colors.black.withOpacity(0.7),
-                textColor: Colors.white,
-                fontSize: 14,
-              );
-            },
+            onPressed: onAlarmPressed ??
+                    () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (_) => const AlarmPage()),
+                  );
+                },
           ),
         ],
       ),
