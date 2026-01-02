54dca8ac366e0a3a76f5f7efb03509a0fffb0400
ej070961 <ej070961@gmail.com>
2026-01-02 20:46:48 +0900

    refactor: 커뮤니티 세그먼트 탭 위젯 분리(#56)

---
+++ lib/feature/community/data/tabs_config.dart
@@ -0,0 +1,24 @@
+import 'package:inninglog/feature/community/screens/teamboard_page.dart';
+import 'package:inninglog/feature/community/widgets/shared/segmented_tabs.dart';
+
+const normalTabs = <CommunityTabItem>[
+  CommunityTabItem(type: BoardTab.onlywan, label: '오직완'),
+  CommunityTabItem(type: BoardTab.free, label: '자유 게시판'),
+  CommunityTabItem(type: BoardTab.news, label: '오늘의 뉴스'),
+];
+
+const myActivityTabs = <CommunityTabItem>[
+  CommunityTabItem(type: BoardTab.onlywan, label: '오직완'),
+  CommunityTabItem(type: BoardTab.free, label: '자유 게시판'),
+];
+
+List<CommunityTabItem> communityTabsByMode(BoardMode mode) {
+  switch (mode) {
+    case BoardMode.normal:
+      return normalTabs;
+    case BoardMode.myPosts:
+    case BoardMode.myComments:
+    case BoardMode.scraps:
+      return myActivityTabs;
+  }
+}
