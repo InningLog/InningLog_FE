b4292caf033b05f85d493afc0f563038a85ff002
ej070961 <ej070961@gmail.com>
2026-01-02 20:49:54 +0900

    refactor: 검색 페이지 path 상수로 분리(#56)

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -6,6 +6,7 @@ final class AppRoutePaths {
 
   static const postWrite = '/post/new'; // GNB 없는 legacy 글쓰기
   static const boardPostWrite = 'post/new'; // /boards/:code 하위 상대경로
+  static const search = '/search';
 
   static String boardPostWriteLocation(String code) => '/boards/$code/post/new';
 }
