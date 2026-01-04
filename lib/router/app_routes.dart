8343225faf93b2fb8fa0a5bbdc37c9330e282a2e
ej070961 <ej070961@gmail.com>
2026-01-04 16:19:17 +0900

    chore: 게시판 페이지 이동 go router 방식으로 수정(#64)

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -6,7 +6,13 @@ final class AppRoutePaths {
 
   static const postWrite = '/post/new'; // GNB 없는 legacy 글쓰기
   static const boardPostWrite = 'post/new'; // /boards/:code 하위 상대경로
+  static const board = '/boards/:code';
   static const search = '/search';
+
+  static String boardLocation(String code, {int? tab}) {
+    final tabQuery = tab != null ? '?tab=$tab' : '';
+    return '/boards/$code$tabQuery';
+  }
 
   static String boardPostWriteLocation(String code) => '/boards/$code/post/new';
 }
