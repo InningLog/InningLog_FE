9bbae6619ecc4c4430cffa1ddd1ce6873d3c84c7
ryujeeKim <ryujee0512@naver.com>
2026-03-03 02:59:20 +0900

    [Feat]마이페이지 구현

---
+++ lib/feature/mypage/viewmodel/my_page_view_model.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:inninglog/feature/user/repositories/user_repository.dart';
+
+class MyPageViewModel extends ChangeNotifier {
+  final UserRepository _repo;
+
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
+  MemberProfileResponse? _profile;
+  MemberProfileResponse? get profile => _profile;
+
+  String? _error;
+  String? get error => _error;
+
+  MyPageViewModel(this._repo);
+
+  Future<void> fetch() async {
+    _isLoading = true;
+    _error = null;
+    notifyListeners();
+
+    try {
+      _profile = await _repo.getProfile();
+    } catch (e) {
+      _error = e.toString();
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
--- lib/feature/user/repositories/user_repository.dart
+++ lib/feature/user/repositories/user_repository.dart
@@ -1,6 +1,35 @@
 import 'package:dio/dio.dart';
 import '../../../shared/network/api_envelope.dart';
 
+class MemberProfileResponse {
+  final String nickname;
+  final String profileUrl;
+  final String teamShortCode;
+  final int totalGameCount;
+  final int winCount;
+  final double winRate;
+
+  MemberProfileResponse({
+    required this.nickname,
+    required this.profileUrl,
+    required this.teamShortCode,
+    required this.totalGameCount,
+    required this.winCount,
+    required this.winRate,
+  });
+
+  factory MemberProfileResponse.fromJson(Map<String, dynamic> json) {
+    return MemberProfileResponse(
+      nickname: json['nickname'] as String? ?? '',
+      profileUrl: json['profileUrl'] as String? ?? '',
+      teamShortCode: json['teamShortCode'] as String? ?? '',
+      totalGameCount: json['totalGameCount'] as int? ?? 0,
+      winCount: json['winCount'] as int? ?? 0,
+      winRate: (json['winRate'] as num?)?.toDouble() ?? 0.0,
+    );
+  }
+}
+
 class UserRepository {
   final Dio _dio;
   UserRepository(this._dio);
@@ -15,5 +44,15 @@ class UserRepository {
       throw const FormatException('Unexpected response shape');
     }
     return ApiEnvelope.fromJson(json);
+  }
+
+  Future<MemberProfileResponse> getProfile() async {
+    final res = await _dio.get('/member/profile');
+    final json = res.data;
+    if (json is! Map<String, dynamic>) {
+      throw const FormatException('Unexpected response shape');
+    }
+    final data = json['data'] as Map<String, dynamic>;
+    return MemberProfileResponse.fromJson(data);
   }
 }
